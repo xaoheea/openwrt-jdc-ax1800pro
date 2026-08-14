cb2fab8427130a030f57e080ff1ab5b64346d2e8
xaoheea <2156076374@qq.com>
2026-08-15 00:33:36 +0800

    feat: switch to OpenClash + add full plugin suite (AdGuard Home, iStore, Docker, SQM, kernel 6.6)

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -5,9 +5,9 @@ echo "DIY Part 1: Add custom feeds"
 # Add Siriling's feed for 5G Modem Support
 echo 'src-git siriling https://github.com/Siriling/5G-Modem-Support.git' >> feeds.conf.default
 
-# Passwall and other feeds are already in feeds.conf.default
-
-# Add any extra logic here if needed
+# OpenClash and iStore feeds are already in feeds.conf.default
+# OpenClash: src-git openclash https://github.com/vernesong/OpenClash.git;dev
+# iStore: src-git istore https://github.com/linkease/istore.git;main
 
 echo "DIY Part 1: Done"
 exit 0
